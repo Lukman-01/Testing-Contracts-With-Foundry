12d1bd52c275a2fe21f8455e0d3b262d9cf996ac
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-09-02 05:14:13 +0100

    test

---
+++ test/Hello.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Hello} from "../src/Hello.sol";
+
+contract HelloTest is Test {
+    Hello public hello;
+
+    function setUp() public {
+        hello = new Hello();
+    }
+    function testGreet() public {
+        assertEq(hello.greet(), "Hello Everyone!");
+    }
+}
